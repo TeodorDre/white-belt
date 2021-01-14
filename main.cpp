cadda6a8ee090b754356974747b52c402701419b
TeodorDre <swen295@gmail.com>
2021-01-15 02:48:41 +0300

    feat(bus-stop): improve search

--- main.cpp
+++ main.cpp
@@ -18,7 +18,26 @@ void PrintAllBuses(const map<string, vector<string>>& bus_and_stops) {
 }
 
 void PrintBusesForStop(const string& bus_stop, const map<string, vector<string>>& bus_and_stops) {
+    vector<string> buses;
 
+    for (const auto& [key, value]: bus_and_stops) {
+        for (const auto& stop: value) {
+            if (stop == bus_stop) {
+                buses.push_back(key);
+                continue;
+            }
+        }
+    }
+
+    if (buses.empty()) {
+        cout << "No stop";
+    } else {
+        for (const auto& bus: buses) {
+            cout << bus << " ";
+        }
+    }
+
+    cout << endl;
 }
 
 void PrintStopsForBus(const string& bus_name, const map<string, vector<string>>& bus_and_stops) {
@@ -28,12 +47,20 @@ void PrintStopsForBus(const string& bus_name, const map<string, vector<string>>&
     }
 }
 
-void AddNewBusStops(const string& bus_name, const vector<string>& stops,  map<string, vector<string>>& bus_and_stops) {
+void AddNewBusStops(const string& bus_name, const vector<string>& stops, map<string, vector<string>>& bus_and_stops) {
+    for (const auto& stop: stops) {
+        bus_and_stops[bus_name].push_back(stop);
+    }
 
+    for (const auto& stop : bus_and_stops[bus_name]) {
+        cout << stop << " ";
+    }
+
+    cout << endl;
 }
 
 int main() {
-    string NEW_BUSES = "NEW_BUSES";
+    string NEW_BUS = "NEW_BUS";
     string BUSES_FOR_STOP = "BUSES_FOR_STOP";
     string STOPS_FOR_BUS = "STOPS_FOR_BUS";
     string ALL_BUSES = "ALL_BUSES";
@@ -46,6 +73,8 @@ int main() {
 
     for (int i = 0; i < operations_n; i++) {
         string command;
+
+        cin >> command;
 
         if (command == ALL_BUSES) {
             PrintAllBuses(bus_and_stops);
@@ -61,7 +90,7 @@ int main() {
             cin >> bus_name;
 
             PrintStopsForBus(bus_name, bus_and_stops);
-        } else if (command == NEW_BUSES) {
+        } else if (command == NEW_BUS) {
             string bus_name;
             int stop_count;
 
@@ -70,7 +99,7 @@ int main() {
 
             vector<string> stops;
 
-            for (int z = 0; i < stop_count; z++) {
+            for (int z = 0; z < stop_count; z++) {
                 string stop_name;
 
                 cin >> stop_name;
