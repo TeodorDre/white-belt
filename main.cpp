9df7e233d3fc30a6fee13c0d791189cd740b3e07
TeodorDre <swen295@gmail.com>
2021-01-26 03:50:39 +0300

    feat(core): processing synonyms

--- main.cpp
+++ main.cpp
@@ -9,16 +9,36 @@
 
 using namespace std;
 
-void Add(string& word1, string& word2,map<string, set<string>>& dict) {
+using Synonyms = map<string, set<string>>;
 
+void Add(const string &word1, const string &word2, Synonyms &dict) {
+    set<string> words;
+
+    words.insert(word1);
+    words.insert(word2);
+
+    dict[word1] = words;
+    dict[word2] = words;
 }
 
-void Count(string& word, const map<string, set<string>>& dic) {
+void Count(const string &word, const Synonyms &dict) {
+    int synonyms = 0;
 
+    for (const auto&[key, value] : dict) {
+        if (value.count(word) == 1) {
+            synonyms++;
+        }
+    }
+
+    cout << synonyms << endl;
 }
 
-void Check(string& word1, string& word2, const map<string, set<string>>& dic) {
+void Check(const string &word1, const string &word2, const Synonyms  &dict) {
+    bool result = false;
 
+    string text = result ? "YES" : "NO";
+
+    cout << text << endl;
 }
 
 int main() {
@@ -26,7 +46,7 @@ int main() {
 
     cin >> operations_n;
 
-    map<string, set<string>> dict;
+    Synonyms dict;
 
     for (int i = 0; i < operations_n; i++) {
         string command;
@@ -53,7 +73,7 @@ int main() {
             cin >> word1;
             cin >> word2;
 
-            Check(word1, word2,dict);
+            Check(word1, word2, dict);
         }
     }
 
