d66ca2e1f91694b7e012f1bb424605f598146724
TeodorDre <swen295@gmail.com>
2021-07-31 22:28:21 +0300

    feat(coursera): fix error

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include <sstream>
-#include <vector>
 #include <set>
-#include <map>
-#include <exception>
 
 using namespace std;
+
 
 int Divider(const int &n, const int &d) {
     if (n == d) {
@@ -38,11 +36,15 @@ int Divider(const int &n, const int &d) {
         } else {
             return divider_t;
         }
+
     }
+
+
 }
 
 
 class Rational {
+
 public:
     Rational() {
         p = 0;
@@ -51,7 +53,7 @@ public:
 
     Rational(int numerator, int denominator) {
         if (denominator == 0) {
-            throw invalid_argument("Invalid argument");
+            throw invalid_argument("ERROR");
         } else {
             Initialize(numerator, denominator);
         }
@@ -80,31 +82,31 @@ public:
         }
     }
 
+
     int Numerator() const {
         return p;
     }
+
 
     int Denominator() const {
         return q;
     }
 
-    const bool operator==(const Rational &a) const {
-        if ((a.p == p)) {
-            return true;
-        } else {
-            return false;
-        }
+
+    bool operator==(const Rational &a) const {
+        return (a.p == p) && (a.q == q);
     }
 
-    const bool operator>(const Rational &a) const {
+    bool operator>(const Rational &a) const {
         if ((p * a.q) > (q * a.p)) {
             return true;
         } else {
             return false;
         }
+
     }
 
-    const bool operator<(const Rational &a) const {
+    bool operator<(const Rational &a) const {
         if ((p * a.q) < (q * a.p)) {
             return true;
         } else {
@@ -126,6 +128,7 @@ public:
         result.SignedFactional();
 
         return result;
+
     }
 
     Rational operator-(const Rational &x) {
@@ -142,7 +145,9 @@ public:
         result.SignedFactional();
 
         return result;
+
     }
+
 
 private:
     int p;
@@ -160,20 +165,17 @@ istream &operator>>(istream &stream, Rational &r) {
     r.Initialize(p_t, q_t);
 
     return stream;
-
 }
 
 
 ostream &operator<<(ostream &stream, const Rational &r) {
     stream << r.Numerator() << '/' << r.Denominator();
-
     return stream;
 }
 
 
 Rational operator*(const Rational &left, const Rational &right) {
     Rational result;
-
     result.Initialize(
             (right.Numerator() * left.Numerator()),
             (right.Denominator() * left.Denominator())
@@ -185,7 +187,7 @@ Rational operator*(const Rational &left, const Rational &right) {
 
 Rational operator/(const Rational &left, const Rational &right) {
     if (right.Numerator() == 0) {
-        throw domain_error("Invalid argument");
+        throw domain_error("WTF?");
     } else {
         Rational result;
 
@@ -196,6 +198,7 @@ Rational operator/(const Rational &left, const Rational &right) {
 
         return result;
     }
+
 }
 
 int main() {
@@ -244,6 +247,125 @@ int main() {
         if (defaultConstructed.Numerator() != 0 || defaultConstructed.Denominator() != 1) {
             cout << "Rational() != 0/1" << endl;
             return 5;
+        }
+    }
+
+    {
+        Rational r1(4, 6);
+        Rational r2(2, 3);
+        bool equal = r1 == r2;
+        if (!equal) {
+            cout << "4/6 != 2/3" << endl;
+            return 1;
+        }
+    }
+
+    {
+        Rational a(2, 3);
+        Rational b(4, 3);
+        Rational c = a + b;
+        bool equal = c == Rational(2, 1);
+        if (!equal) {
+            cout << "2/3 + 4/3 != 2" << endl;
+            return 2;
+        }
+    }
+
+    {
+        Rational a(5, 7);
+        Rational b(2, 9);
+        Rational c = a - b;
+        bool equal = c == Rational(31, 63);
+        if (!equal) {
+            cout << "5/7 - 2/9 != 31/63" << endl;
+            return 3;
+        }
+    }
+
+    {
+        Rational a(2, 3);
+        Rational b(4, 3);
+        Rational c = a * b;
+        bool equal = c == Rational(8, 9);
+        if (!equal) {
+            cout << "2/3 * 4/3 != 8/9" << endl;
+            return 1;
+        }
+    }
+
+    {
+        Rational a(5, 4);
+        Rational b(15, 8);
+        Rational c = a / b;
+        bool equal = c == Rational(2, 3);
+        if (!equal) {
+            cout << "5/4 / 15/8 != 2/3" << endl;
+            return 2;
+        }
+    }
+
+    {
+        ostringstream output;
+        output << Rational(-6, 8);
+        if (output.str() != "-3/4") {
+            cout << "Rational(-6, 8) should be written as \"-3/4\"" << endl;
+            return 1;
+        }
+    }
+
+    {
+        istringstream input("5/7");
+        Rational r;
+        input >> r;
+        bool equal = r == Rational(5, 7);
+        if (!equal) {
+            cout << "5/7 is incorrectly read as " << r << endl;
+            return 2;
+        }
+    }
+
+    {
+        istringstream input("");
+        Rational r;
+        bool correct = !(input >> r);
+        if (!correct) {
+            cout << "Read from empty stream works incorrectly" << endl;
+            return 3;
+        }
+    }
+
+    {
+        istringstream input("5/7 10/8");
+        Rational r1, r2;
+        input >> r1 >> r2;
+        bool correct = r1 == Rational(5, 7) && r2 == Rational(5, 4);
+        if (!correct) {
+            cout << "Multiple values are read incorrectly: " << r1 << " " << r2 << endl;
+            return 4;
+        }
+
+        input >> r1;
+        input >> r2;
+
+        correct = r1 == Rational(5, 7) && r2 == Rational(5, 4);
+        if (!correct) {
+            cout << "Read from empty stream shouldn't change arguments: " << r1 << " " << r2 << endl;
+            return 5;
+        }
+    }
+
+    {
+        istringstream input1("1*2"), input2("1/"), input3("/4");
+        Rational r1, r2, r3;
+        input1 >> r1;
+        input2 >> r2;
+        input3 >> r3;
+        bool correct = r1 == Rational() && r2 == Rational() && r3 == Rational();
+        if (!correct) {
+            cout << "Reading of incorrectly formatted rationals shouldn't change arguments: "
+            << r1 << " " << r2 << " " << r3 << endl;
+
+            return 6;
         }
     }
 
