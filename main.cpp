4aa02b638e2bc594f683a49ac873681bda8ef6be
TeodorDre <swen295@gmail.com>
2021-09-25 16:33:12 +0300

    feat(white-belt): add final objective

--- main.cpp
+++ main.cpp
@@ -2,6 +2,33 @@
 // Created by Andrew Slesarenko on 25.09.2021.
 //
 
+class Date {
+    Date(int y, int m, int d) {
+        year = y;
+        month = m;
+        day = d;
+    }
+
+private:
+    int year;
+    int month;
+    int day;
+};
+
+class Database {
+public:
+    void Add();
+
+    void Remove();
+
+    void RemoveMany();
+
+    void Find();
+
+    void PrintAll();
+
+};
+
 int main() {
     return 0;
 }
